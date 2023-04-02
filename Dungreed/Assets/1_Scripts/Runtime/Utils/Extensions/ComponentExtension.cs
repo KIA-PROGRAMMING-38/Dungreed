557e82c67f93bbb571b6db1329f9d95a64ca5a18
Nameless1004 <jjho1029@gmail.com>
2023-04-02 21:02:11 +0900

    Feat: Component 확장 메서드 추가

---
+++ Dungreed/Assets/1_Scripts/Runtime/Utils/Extensions/ComponentExtension.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public static class ComponentExtension
+{
+
+    /// <summary>
+    /// 현재 오브젝트 -> 자식 오브젝트 -> 부모 오브젝트 순으로 컴포넌트를 찾습니다.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="owner"></param>
+    /// <returns></returns>
+    public static T GetComponentAllCheck<T>(this Component owner) where T : Component
+    {
+        T comp = owner.gameObject.GetComponent<T>();
+
+        if (comp == null)
+        {
+            comp = owner.gameObject.GetComponentInChildren<T>();
+
+            if (comp == null)
+            {
+                comp = owner.gameObject.GetComponentInParent<T>();
+            }
+        }
+
+
+        return comp;
+    }
+}
