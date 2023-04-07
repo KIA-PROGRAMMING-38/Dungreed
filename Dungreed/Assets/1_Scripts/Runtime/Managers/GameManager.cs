17ebeec6f781c01fc925a7d9375a999b18a9013a
Nameless1004 <jjho1029@gmail.com>
2023-04-07 17:10:52 +0900

    Feat: 발사체 풀러 클래스 게임매니저에 추가

--- Dungreed/Assets/1_Scripts/Runtime/Managers/GameManager.cs
+++ Dungreed/Assets/1_Scripts/Runtime/Managers/GameManager.cs
@@ -8,16 +8,20 @@ public class GameManager : Singleton<GameManager>
     [field: SerializeField]
     public FxPooler FxPooler { get; private set; }
     [field: SerializeField]
+    public ProjectTilePooler ProjectTilePooler { get; private set; }
+    [field: SerializeField]
     public WeaponDataManager WeaponDataManager { get; private set; }
 
     protected override void Awake()
     {
         base.Awake();
+        WeaponDataManager = new WeaponDataManager();
     }
 
     protected void OnDestroy()
     {
         FxPooler = null;
+        WeaponDataManager = null;
     }
 
 }
