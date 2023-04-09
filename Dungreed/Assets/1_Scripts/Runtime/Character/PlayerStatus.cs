71d7af950be9e6bbd20f47838635a3e75540edd7
Nameless1004 <jjho1029@gmail.com>
2023-04-09 21:20:53 +0900

    Feat: 스테이터스 클래스 추가

---
+++ Dungreed/Assets/1_Scripts/Runtime/Character/PlayerStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+[Serializable]
+public class PlayerStatus : Status
+{
+    public float DashDamage;
+    public int MaxDashCount;
+    public float CriticalChance;
+    public float CriticalDamage;
+    public float Evade;
+    public float Exp;
+    public float Level;
+}
---
+++ Dungreed/Assets/1_Scripts/Runtime/Character/Status.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class Status
+{
+    public float AttackPower;
+    public float MoveSpeed;
+    public float Defense;
+    public float JumpPower;
+    public int MaxHp;
+}
