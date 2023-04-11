a725ef5f5ac4e12c7ed7fae37c584003b83b43d5
Nameless1004 <jjho1029@gmail.com>
2023-04-11 17:52:57 +0900

    Feat: 트리거 시스템 구현

---
+++ Dungreed/Assets/1_Scripts/Runtime/Level/Trigger/DelayTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DelayTrigger : Trigger
+{
+    [SerializeField] protected float _delayTime;
+    protected bool _delayTrigger;
+    protected IEnumerator DelayInvokeCoroutine;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        DelayInvokeCoroutine = DelayInvoke();
+    }
+
+    protected override void TriggerEnter()
+    {
+        StartCoroutine(DelayInvokeCoroutine);
+    }
+
+    protected override void TriggerExit()
+    {
+        _ExitAction?.Invoke();
+    }
+
+    private IEnumerator DelayInvoke()
+    {
+        _state = TriggerState.Disable;
+        yield return YieldCache.WaitForSeconds(_delayTime);
+        _EnterAction?.Invoke();
+    }
+}
